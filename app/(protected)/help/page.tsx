0807b9f29d7541c1223221fff954ee20f09f0b0d
arfiandys <222011791@stis.ac.id>
2024-05-30 08:06:30 +0700

    menambah buku panduan

--- app/(protected)/help/page.tsx
+++ app/(protected)/help/page.tsx
@@ -1,13 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { HelpCircle, MessageCircleQuestion } from "lucide-react";
+import Link from "next/link";
 
 const HelpPage = () => {
-    return ( 
+    return (
         <div className="flex justify-center items-center h-[80vh] w-full">
-            <Button variant="default" size="sm">
-                Help!
-            </Button>
+            <Link href="/Guide_AKIP-Evaluasi_1.0.pdf" legacyBehavior>
+                <a target="_blank">
+                    <Button variant="default">
+                        <span>Buku panduan</span>
+                        <MessageCircleQuestion className="h-4 w-4 ml-2" />
+                    </Button>
+                </a>
+            </Link>
         </div>
-     );
+    );
 }
- 
+
 export default HelpPage;
